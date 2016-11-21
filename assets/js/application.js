fc05fef3156d6966f4e27bd6ef78f806f7655082
Seriously Silly <basia_kujawska@outlook.com>
2016-11-21 19:48:06 +0100

    Styling, smooth scroll, hero fade-out, toggle animation and closing

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,31 @@
+//fading brand-heading
+$(window).scroll(function(){
+  $(".fade-out").css("opacity", 1 - $(window).scrollTop() / 500);
+});
+
+//smooth scrolling
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
+//hamburger animation
 $(document).ready(function () {
         $(".navbar-toggle").on("click", function () {
             $(this).toggleClass("active");
         });
     });
+
+//close hamburger on click
+$(".navbar-nav li a").click(function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+});
