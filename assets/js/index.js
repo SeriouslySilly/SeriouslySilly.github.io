d320500d9ece2c3ae8bc41157c947a3f5c572041
Seriously Silly <basia_kujawska@outlook.com>
2016-12-28 22:20:26 +0100

    Longer reference carousel interval

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,7 +44,7 @@ $(".navbar-nav li a").click(function(event) {
 $(document).ready(function() {
   //carousel options
   $('#quote-carousel').carousel({
-    pause: true, interval: 10000,
+    pause: true, interval: 18000,
   });
 });
 
